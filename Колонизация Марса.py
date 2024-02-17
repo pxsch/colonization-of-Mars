7ee825df5b57d679013a028f65abf6ff4532c98d
Lesha <qu4rtz43@gmail.com>
2024-02-17 17:13:55 +0300

    add "/image_mars"

--- Колонизация Марса.py
+++ Колонизация Марса.py
@@ -23,5 +23,22 @@ def promotion():
     return "<br>".join(text)
 
 
+@app.route('/image_mars')
+def mars_image():
+    return f"""<!doctype html>
+                 <html lang="en">
+                   <head>
+                     <meta charset="utf-8">
+                     <title>Привет, Марс!</title>
+                   </head>
+                   <body>
+                     <h1>Жди нас, Марс!</h1>
+                     <img src="{url_for('static', filename='img/mars.png')}"
+                     alt="здесь должна была быть картинка, но не нашлась">
+                     <p>Вот она какая, красная планета</p>
+                   </body>
+                 </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
