5b407f95d6438e919aa778dbfe76c2ec7e9a0ed2
Lesha <qu4rtz43@gmail.com>
2024-03-03 21:21:08 +0300

    add "/choice/<planet_name>"

--- Колонизация Марса.py
+++ Колонизация Марса.py
@@ -211,5 +211,40 @@ def form_sample():
         return "Форма отправлена"
 
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f"""<!doctype html>
+                 <html lang="en">
+                   <head>
+                     <meta charset="utf-8">
+                     <meta name="viewport" content="width=device-width, initial-scale=1">
+                     <link rel="stylesheet" 
+                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                     crossorigin="anonymous">   
+                     <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                     <title>Варианты выбора</title>
+                   </head>
+                   <body>
+                     <h1>Моё предложение: {planet_name}</h1>
+                     <div class="alert alert-secondary" role="alert">
+                       Эта планета близка к Земле;
+                     </div>
+                     <div class="alert alert-success" role="alert">
+                       На ней много необходимых ресурсов;
+                     </div>
+                     <div class="alert alert-info" role="alert">
+                       На ней есть вода и атмосфера;
+                     </div>
+                     <div class="alert alert-warning" role="alert">
+                       На ней есть небольшое магнитное поле;
+                     </div>
+                     <div class="alert alert-danger" role="alert">
+                       Наконец, она просто красива!
+                     </div>
+                   </body>
+                 </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
