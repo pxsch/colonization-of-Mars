f566d33c30aec46f3a7590cf3e931ae401a0ac72
Lesha <qu4rtz43@gmail.com>
2024-03-12 22:25:36 +0300

    add "/results/<nickname>/<int:level>/<float:rating>"

--- Колонизация Марса.py
+++ Колонизация Марса.py
@@ -246,5 +246,36 @@ def choice(planet_name):
                  </html>"""
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                 <html lang="en">
+                   <head>
+                     <meta charset="utf-8">
+                     <meta name="viewport" content="width=device-width, initial-scale=1">
+                     <link rel="stylesheet" 
+                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                     crossorigin="anonymous">   
+                     <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                     <title>Результаты</title>
+                   </head>
+                   <body>
+                     <h1>Результаты отбора</h1>
+                     <div class="alert alert-secondary" role="alert">
+                       Претендента на участие в миссии {nickname}:
+                     </div>
+                     <div class="alert alert-success" role="alert">
+                       Поздравляем! Ваш рейтинг после {level} этапа отбора:
+                     </div>
+                     <div class="alert alert-info" role="alert">
+                       Составляет {rating}!
+                     </div>
+                     <div class="alert alert-warning" role="alert">
+                       Желаем удачи!
+                   </body>
+                 </html>"""
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
